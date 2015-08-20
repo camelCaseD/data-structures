f725100ff22e584c58552f9a273c2df8eadee273
camelCaseD <brandon@camelcased.com>
2015-08-19 17:23:45 -0700

    Start linkedLists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,6 +4,18 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.tail === null) {
+      list.tail = Node(value);
+    } else if (list.head === null) {
+      list.head = list.tail;
+
+      var node = Node(value);
+      list.head.next = node;
+
+      list.tail = node;
+    } else {
+      
+    }
   };
 
   list.removeHead = function(){
