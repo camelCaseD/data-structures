b78d0ea1e7ee2260ce3d78f8375c6560a5dd8085
camelCaseD <brandon@camelcased.com>
2015-08-21 11:57:47 -0700

    Finish binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,62 @@
 var BinarySearchTree = function(value){
+  var tree = {};
+
+  tree.value = value;
+
+  tree.left = null;
+
+  tree.right = null;
+
+  tree.contains = function(value) {
+    if(tree.value === value) {
+      return true;
+    } else if (tree.value > value && tree.left !== null) {
+      return tree.left.contains(value);
+    } else if (tree.right !== null) {
+      return tree.right.contains(value);
+    }
+    return false;
+  }
+
+  tree.depthFirstLog = function(func) {
+    func(tree.value);
+
+    if (tree.left !== null) {
+      tree.left.depthFirstLog(func);
+    }
+
+    if (tree.right !== null) {
+      tree.right.depthFirstLog(func);
+    }
+  }
+
+  tree.insert = function(value) {
+    if (tree.left === null && tree.value > value) {
+      tree.left = BinarySearchTree(value);
+
+      return true;
+    } else if(tree.right === null && tree.value < value) {
+
+      tree.right = BinarySearchTree(value);
+
+      return true;
+    } else {
+      if (tree.value > value) {
+        tree.left.insert(value);
+      } else {
+        tree.right.insert(value);
+      }
+
+      return true;
+    }
+
+    return false;
+  } 
+
+  return tree;
 };
+
+
 
 
 /*
